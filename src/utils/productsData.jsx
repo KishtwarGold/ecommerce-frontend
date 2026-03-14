67f0ccd72e3424a1c583526d083b368edb69b3cd
Khushi <khushi0712042@gmail.com>
2026-03-15 01:20:30 +0530

    final

--- src/utils/productsData.jsx
+++ src/utils/productsData.jsx
@@ -15,7 +15,7 @@ export const products = [
     productType: "Saffron",
     category: "Premium Collection",
     weight: "1g",
-    price:650,
+    price:5,
     image: image1,
     description: [
       "A total of 1 gram of premium saffron presented in an elegant gift box, making it a perfect choice for festive and special occasions.",
